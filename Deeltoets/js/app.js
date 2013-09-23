bd12034c1fd3eb14d96c1a0c09656bcf8726741d
sjoerdvalk1991 <sjoerdvalk@086.207.028.145.hva.nl>
2013-09-23 16:17:44 +0200

    update deeltoets

--- Deeltoets/js/app.js
+++ Deeltoets/js/app.js
@@ -19,15 +19,47 @@ var SCOREAPP = SCOREAPP || {};
         ]
     };
 
-    SCOREAPP.page2 = {
-        title:'Pagina 2',
-        description:'Pagina 2 is de tweede pagina'
-    };
+     SCOREAPP.game = {
+         title:'game',
+         description:'game is de tweede pagina',
+         items: [
+    { score: "1", team1: "Boomsquad", team1Score: "1", team2: "Burning Snow", team2Score: "0"},
+    { score: "2", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "0"},
+    { score: "3", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "1"},
+    { score: "4", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "2"},
+    { score: "5", team1: "Boomsquad", team1Score: "3", team2: "Burning Snow", team2Score: "2"},
+    { score: "6", team1: "Boomsquad", team1Score: "4", team2: "Burning Snow", team2Score: "2"},
+    { score: "7", team1: "Boomsquad", team1Score: "5", team2: "Burning Snow", team2Score: "2"},
+    { score: "8", team1: "Boomsquad", team1Score: "5", team2: "Burning Snow", team2Score: "3"},
+    { score: "9", team1: "Boomsquad", team1Score: "6", team2: "Burning Snow", team2Score: "3"},
+    { score: "10", team1: "Boomsquad", team1Score: "7", team2: "Burning Snow", team2Score: "3"},
+    { score: "11", team1: "Boomsquad", team1Score: "7", team2: "Burning Snow", team2Score: "4"},
+    { score: "12", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "4"},
+    { score: "13", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "5"},
+    { score: "14", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "6"},
+    { score: "15", team1: "Boomsquad", team1Score: "9", team2: "Burning Snow", team2Score: "6"},
+    { score: "16", team1: "Boomsquad", team1Score: "9", team2: "Burning Snow", team2Score: "7"},
+    { score: "17", team1: "Boomsquad", team1Score: "10", team2: "Burning Snow", team2Score: "7"},
+    { score: "18", team1: "Boomsquad", team1Score: "11", team2: "Burning Snow", team2Score: "7"},
+    { score: "19", team1: "Boomsquad", team1Score: "12", team2: "Burning Snow", team2Score: "7"},
+    { score: "20", team1: "Boomsquad", team1Score: "13", team2: "Burning Snow", team2Score: "7"},
+    { score: "21", team1: "Boomsquad", team1Score: "14", team2: "Burning Snow", team2Score: "7"},
+    { score: "22", team1: "Boomsquad", team1Score: "14", team2: "Burning Snow", team2Score: "8"},
+    { score: "23", team1: "Boomsquad", team1Score: "15", team2: "Burning Snow", team2Score: "8"}
+    ]
+     };
 
-    SCOREAPP.page3 = {
-        title:'Pagina 3',
-        description:'Pagina 3 is de derde pagina'
-    };
+     SCOREAPP.ranking = {
+         title:'ranking',
+         description:'ranking is de tweede pagina',
+         items: [
+    { team: "Chasing", Win: "2", Lost: "2", Sw: "7", Sl: "9", Pw: "35", Pl: "39"},
+    { team: "Boomsquad", Win: "2", Lost: "2", Sw: "9", Sl: "8", Pw: "36", Pl: "34"},
+    { team: "Burning Snow", Win: "3", Lost: "1", Sw: "11", Sl: "4", Pw: "36", Pl: "23"},
+    { team: "Beast Amsterdam", Win: "2", Lost: "2", Sw: "6", Sl: "8", Pw: "30", Pl: "34"},
+    { team: "Amsterdam Money Gang", Win: "1", Lost: "3", Sw: "6", Sl: "10", Pw: "30", Pl: "37"}
+    ]
+     };
 
     // Controller Init
     SCOREAPP.controller = {
@@ -44,12 +76,12 @@ var SCOREAPP = SCOREAPP || {};
                 '/schedule': function() {
                     SCOREAPP.page.render('schedule');
                 },
-                '/page2': function() {
-                    SCOREAPP.page.render('page2');
+                '/ranking': function() {
+                    SCOREAPP.page.render('ranking');
                 },
 
-                '/page3': function() {
-                    SCOREAPP.page.render('page3');
+                '/game': function() {
+                    SCOREAPP.page.render('game');
                 },
                 '*': function() {
                     SCOREAPP.page.render('schedule');
